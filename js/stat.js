e666b8af8f798b8c8119f4f8968becee01ec6f15
manya13 <zmeika06@list.ru>
2018-11-20 17:57:11 +0300

    Исправления 20.11

--- js/stat.js
+++ js/stat.js
@@ -10,7 +10,7 @@ var BAR_GAP = 50;
 var TOP_GAP = 250;
 var FONT_HEIGHT = 18;
 var BAR_WIDTH = 40;
-var barHeight = -150;
+var BAR_HEIGHT = 150;
 
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
@@ -29,6 +29,22 @@ var getMaxElement = function (arr) {
   return maxElement;
 };
 
+var getStatisticsXPosition = function (serialNumber) {
+  return CLOUD_X + LEFT_GAP + (BAR_WIDTH + BAR_GAP) * serialNumber;
+};
+
+var getStatisticsYPosition = function (numberOfIndents) {
+  return TOP_GAP - GAP * numberOfIndents;
+};
+
+var getTextXPosition = function () {
+  return CLOUD_X + GAP * 2;
+};
+
+var getTextYPosition = function () {
+  return CLOUD_Y * 4;
+};
+
 window.renderStatistics = function (ctx, players, times) {
   renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
@@ -39,18 +55,15 @@ window.renderStatistics = function (ctx, players, times) {
 
   for (var i = 0; i < players.length; i++) {
     ctx.font = '16px PT Mono';
-    ctx.fillText('Ура вы победили!', CLOUD_X + GAP * 2, CLOUD_Y * 4);
-    ctx.fillText('Список результатов:', CLOUD_X + GAP * 2, CLOUD_Y * 4 + FONT_HEIGHT);
-    ctx.fillText(Math.round(times[i]), CLOUD_X + LEFT_GAP + (BAR_WIDTH + BAR_GAP) * i, TOP_GAP - GAP * 2 + (barHeight * times[i]) / maxTime);
-    ctx.globalAlpha = Math.random() + 0.1;
-    ctx.fillStyle = 'blue';
+    ctx.fillText('Ура вы победили!', getTextXPosition(), getTextYPosition());
+    ctx.fillText('Список результатов:', getTextXPosition(), getTextYPosition() + FONT_HEIGHT);
+    ctx.fillText(Math.round(times[i]), getStatisticsXPosition(i), getStatisticsYPosition(2) + (-BAR_HEIGHT * times[i]) / maxTime);
+    ctx.fillStyle = 'rgba(0, 0, 255, ' + Math.random().toFixed(1) + ')';
     if (players[i] === 'Вы') {
-      ctx.globalAlpha = 1;
       ctx.fillStyle = 'rgba(255, 0, 0, 1)';
     }
-    ctx.fillRect(CLOUD_X + LEFT_GAP + (BAR_WIDTH + BAR_GAP) * i, TOP_GAP - GAP, BAR_WIDTH, (barHeight * times[i]) / maxTime);
-    ctx.globalAlpha = 1;
+    ctx.fillRect(getStatisticsXPosition(i), getStatisticsYPosition(1), BAR_WIDTH, (-BAR_HEIGHT * times[i]) / maxTime);
     ctx.fillStyle = '#000';
-    ctx.fillText(players[i], CLOUD_X + LEFT_GAP + (BAR_WIDTH + BAR_GAP) * i, CLOUD_Y + TOP_GAP);
+    ctx.fillText(players[i], getStatisticsXPosition(i), CLOUD_Y + TOP_GAP);
   }
 };
