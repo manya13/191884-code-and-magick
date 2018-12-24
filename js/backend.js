87e5c7d660ec5fc6c91a319ba4e73be1f3edd2a1
manya13 <zmeika06@list.ru>
2018-12-24 14:29:22 +0300

    Учебный проект: pimp my mage

---
+++ js/backend.js
@@ -0,0 +1,56 @@
+'use strict';
+
+(function () {
+
+  var URL_FORM = 'https://js.dump.academy/code-and-magick';
+  var URL_DATA = 'https://js.dump.academy/code-and-magick/data';
+
+  // отправка данных на сервер
+
+  var save = function (data, onLoad, onError) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+
+    xhr.addEventListener('load', function () {
+      if (xhr.status === 200) {
+        onLoad(xhr.response);
+      } else {
+        onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+      }
+    });
+
+    xhr.open('POST', URL_FORM);
+    xhr.send(data);
+  };
+
+  // загрузка данных
+
+  var load = function (onLoad, onError) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+
+    xhr.addEventListener('load', function () {
+      if (xhr.status === 200) {
+        onLoad(xhr.response);
+      } else {
+        onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+      }
+    });
+    xhr.addEventListener('error', function () {
+      onError('Произошла ошибка соединения');
+    });
+    xhr.addEventListener('timeout', function () {
+      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+    });
+
+    xhr.timeout = 10000;
+
+    xhr.open('GET', URL_DATA);
+    xhr.send();
+  };
+
+  window.backend = {
+    save: save,
+    load: load
+  };
+})();
